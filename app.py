44ff2029e7c094135ca3615a6e03fb9fa06ed962
numbush <gkrainis.work@gmail.com>
2026-06-18 17:11:50 +0300

    add fastapi with uvicorn

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask
+from fastapi import FastAPI
+import uvicorn
+import threading
 
-app = Flask(__name__)
+# Flask app
+flask_app = Flask(__name__)
 
-@app.route('/')
-def home() :
-    return " Hello from jenkins and Flask"
+@flask_app.route('/')
+def home():
+    return "Hello from Jenkins + Flask!"
 
-@app.route('/helath')
-def health() :
-    return {"status" : "ok"}
+@flask_app.route('/health')
+def health():
+    return {"status": "ok"}
 
-if __name__== '__main__':
-    app.run(host='0.0.0.0' , port=5000)
+# FastAPI app
+fastapi_app = FastAPI()
+
+@fastapi_app.get('/')
+def fastapi_home():
+    return {"message": "Hello from Jenkins + FastAPI!"}
+
+@fastapi_app.get('/health')
+def fastapi_health():
+    return {"status": "ok"}
+
+def run_flask():
+    flask_app.run(host='0.0.0.0', port=5000)
+
+def run_fastapi():
+    uvicorn.run(fastapi_app, host='0.0.0.0', port=8000)
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=run_flask)
+    t2 = threading.Thread(target=run_fastapi)
+    t1.start()
+    t2.start()
